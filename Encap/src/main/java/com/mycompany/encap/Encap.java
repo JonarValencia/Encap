dad8efcff4c2942cb435cff61b6e6bb5b29eacc4
JonarValencia <jonar07kambal@gmail.com>
2026-02-26 23:48:23 +0800

    commit

---
+++ Encap/src/main/java/com/mycompany/encap/Encap.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.encap;
+
+/**
+ *
+ * @author ruthe
+ */
+public class Encap {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
